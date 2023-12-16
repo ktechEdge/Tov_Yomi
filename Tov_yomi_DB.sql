5fd5223868227233937e6d2171b44fcbf3779b98
ZionAmar <zion0549774827@hotmail.com>
2023-12-16 20:28:44 +0200

    עדכנתי את קובץ הדאטהבייס

--- Tov_yomi_DB.sql
+++ Tov_yomi_DB.sql
@@ -4,9 +4,33 @@ CREATE DATABASE IF NOT EXISTS Tov_yomi;
 -- השימוש במסד נתונים Tov_yomi
 USE Tov_yomi;
 
+-- --------------------------------------------------------
+
 -- יצירת טבלה Users
 CREATE TABLE IF NOT EXISTS Users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR(255),
-    Email VARCHAR(255)
+    id INT(20) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    First_name VARCHAR(255) DEFAULT NULL,
+    Last_name varchar(255) DEFAULT NULL,
+    Email varchar(255) DEFAULT NULL,
+    User_name varchar(255) DEFAULT NULL,
+    Password int(222) NOT NULL
 );
+
+-- --------------------------------------------------------
+
+-- יצירת טבלה items
+CREATE TABLE IF NOT EXISTS items (
+   id int(11) NOT NULL,
+   item text NOT NULL,
+   Date date NOT NULL
+);
+
+-- --------------------------------------------------------
+
+-- יצירת טבלה settings
+CREATE TABLE IF NOT EXISTS settings (
+   Image_URL varchar(255) DEFAULT NULL,
+   to_remind tinyint(1) NOT NULL,
+   Reminder_time time NOT NULL
+);
+
