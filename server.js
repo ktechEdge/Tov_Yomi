50d7822139c3e70c4b369fe614c3bcd2f7cdc911
MichaelChemo2507 <mnh8551@gmail.com>
2023-12-21 19:30:18 +0200

    finish working on the page design

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const app = express();
+const port = 5959;
+
 const bodyParser = require('body-parser');
 const path = require("path");
-app.use(express.static(path.join(__dirname)));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, "css")));
+app.use(express.static(path.join(__dirname, "js")));
 app.set("view engine","ejs");
+
 let db_M = require('./database');
 global.db_pool = db_M.pool;
-const port = 5959;
 app.use(express.json());
+
+
 app.listen(port, () => {
     console.log(`Now listening on port http://localhost:${port}`);
 });
